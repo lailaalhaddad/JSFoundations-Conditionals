296c07dfe81df739e06a717e823305c5923c61e2
lailaalhaddad <lalhaddad95@gmail.com>
2021-02-02 18:17:14 +0300

    Solved the conditionals task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 // Define the numbers and the operation here
-
+let num1 = 2;
+let num2 = 8;
+let op = "*";
 // Write if-statements here
+if (op == "*") {
+    console.log( num1 + op + num2 + " = 16"); 
+} else{
+    console.log("operation failed")
+}
